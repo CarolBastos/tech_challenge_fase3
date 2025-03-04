f5aec20333ec7b68d5dcbe6ee10fb2b2b99a43ec
Carol Bastos <bastoscarol292@gmail.com>
2025-03-04 19:39:58 -0400

    setup inicial

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../app_colors.dart';
+import '../routes.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: const Color(0xFFF7F3FC)),
+      backgroundColor: const Color(0xFFF7F3FC), // Light purple background
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Welcome page',
+                  style: TextStyle(
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primary[800],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
