1540f6e06a22b354f7b09c6cbf7670401996364c
Tayná Martins Ramos <tayna.ramos@ifood.com.br>
2025-05-19 22:17:43 -0300

    rotação de chaves

--- lib/utils/generate_key.dart
+++ lib/utils/generate_key.dart
@@ -4,37 +4,53 @@ import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 final _secureStorage = const FlutterSecureStorage();
-const _aesKeyStorageKey = 'aes_key';
 
-Future<encrypt.Key> _getOrCreateAESKey() async {
-  String? base64Key = await _secureStorage.read(key: _aesKeyStorageKey);
+const _currentKeyId = 'v1';
+const _keyPrefix = 'aes_key_';
 
-  if (base64Key == null) {
-    final key = encrypt.Key.fromSecureRandom(32); // AES 256
-    base64Key = base64Encode(key.bytes);
-    await _secureStorage.write(key: _aesKeyStorageKey, value: base64Key);
+Future<encrypt.Key> _getKey(String keyId) async {
+  final base64Key = await _secureStorage.read(key: '$_keyPrefix$keyId');
+  if (base64Key == null) throw Exception('Chave $keyId não encontrada');
+  return encrypt.Key(base64Decode(base64Key));
+}
+
+Future<encrypt.Key> _getOrCreateCurrentKey() async {
+  final currentKey = await _secureStorage.read(key: '$_keyPrefix$_currentKeyId');
+
+  if (currentKey != null) {
+    return encrypt.Key(base64Decode(currentKey));
+  } else {
+    final key = encrypt.Key.fromSecureRandom(32);
+    await _secureStorage.write(
+      key: '$_keyPrefix$_currentKeyId',
+      value: base64Encode(key.bytes),
+    );
     return key;
   }
-
-  final keyBytes = base64Decode(base64Key);
-  return encrypt.Key(keyBytes);
 }
 
 Future<String> encryptText(String plainText) async {
-  final key = await _getOrCreateAESKey();
-  final iv = encrypt.IV.fromLength(16);
+  final key = await _getOrCreateCurrentKey();
+  final iv = encrypt.IV.fromSecureRandom(16);
   final encrypter = encrypt.Encrypter(encrypt.AES(key));
 
   final encrypted = encrypter.encrypt(plainText, iv: iv);
-  return encrypted.base64;
+
+  return jsonEncode({
+    'keyId': _currentKeyId,
+    'iv': iv.base64,
+    'data': encrypted.base64,
+  });
 }
 
 Future<String> decryptText(String encryptedText) async {
-  final key = await _getOrCreateAESKey();
-  final iv = encrypt.IV.fromLength(16);
-  final encrypter = encrypt.Encrypter(encrypt.AES(key));
+  final decoded = jsonDecode(encryptedText);
+  final keyId = decoded['keyId'];
+  final iv = encrypt.IV.fromBase64(decoded['iv']);
+  final data = decoded['data'];
 
-  final decrypted = encrypter.decrypt64(encryptedText, iv: iv);
-  return decrypted;
+  final key = await _getKey(keyId);
+  final encrypter = encrypt.Encrypter(encrypt.AES(key));
+  return encrypter.decrypt64(data, iv: iv);
 }
 
