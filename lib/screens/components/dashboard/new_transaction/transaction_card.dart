c520bf73f38c43608d7a230d04049f739e472b7c
ig0rfranca <igorruizdefranca@gmail.com>
2025-05-21 00:13:08 -0300

    Melhoria do lazy loading na listagem de transações e novas transações

--- lib/screens/components/dashboard/new_transaction/transaction_card.dart
+++ lib/screens/components/dashboard/new_transaction/transaction_card.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:tech_challenge_fase3/app_colors.dart';
 import 'package:tech_challenge_fase3/screens/components/dashboard/new_transaction/new_transaction.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
-class TransactionCard extends StatelessWidget {
+class TransactionCard extends StatefulWidget {
   const TransactionCard({super.key});
 
   @override
+  State<TransactionCard> createState() => _TransactionCardState();
+}
+
+class _TransactionCardState extends State<TransactionCard> {
+  bool _isVisible = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      color: AppColors.gray,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: NewTransaction(),
+    return VisibilityDetector(
+      key: Key('transaction-card'),
+      onVisibilityChanged: (info) {
+        if (!_isVisible && info.visibleFraction > 0) {
+          setState(() {
+            _isVisible = true;
+          });
+        }
+      },
+      child: Card(
+        elevation: 0,
+        color: AppColors.gray,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: _isVisible ? const NewTransaction() : const SizedBox(height: 200),
+        ),
       ),
     );
   }
