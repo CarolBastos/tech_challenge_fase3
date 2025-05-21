4d2ef386a0591f294321fb3fcc0e51a1c0203410
ig0rfranca <igorruizdefranca@gmail.com>
2025-05-20 23:29:44 -0300

    Funcionalidade de`visualizar senha no form de login e criar usuário

--- lib/screens/components/custom_text_field.dart
+++ lib/screens/components/custom_text_field.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
+
 import '../../app_colors.dart';
 
 class CustomTextField extends StatefulWidget {
   final String labelText;
   final String placeholder;
   final bool isPassword;
-  final TextEditingController controller; // Adicionando o controller
+  final TextEditingController controller;
 
   const CustomTextField({
-    Key? key,
+    super.key,
     required this.labelText,
     required this.placeholder,
     this.isPassword = false,
     required this.controller,
-  }) : super(key: key);
+  });
 
   @override
   _CustomTextFieldState createState() => _CustomTextFieldState();
 }
 
 class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -32,18 +47,27 @@ class _CustomTextFieldState extends State<CustomTextField> {
         const SizedBox(height: 6),
         TextFormField(
           controller: widget.controller,
+          obscureText: widget.isPassword ? _obscureText : false,
           decoration: InputDecoration(
             hintText: widget.placeholder,
             hintStyle: const TextStyle(color: AppColors.greyPlaceholder),
             border: const OutlineInputBorder(),
-            enabledBorder: OutlineInputBorder(
+            enabledBorder: const OutlineInputBorder(
               borderSide: BorderSide(color: AppColors.grey),
             ),
-            focusedBorder: OutlineInputBorder(
+            focusedBorder: const OutlineInputBorder(
               borderSide: BorderSide(color: AppColors.grey, width: 2),
             ),
+            suffixIcon: widget.isPassword
+                ? IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: const Color.fromARGB(255, 110, 110, 110),
+                    ),
+                    onPressed: _toggleVisibility,
+                  )
+                : null,
           ),
-          obscureText: widget.isPassword,
         ),
       ],
     );
